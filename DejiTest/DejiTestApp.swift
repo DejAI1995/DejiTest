e8c43a1f22b05325e4ef4d4f20658599cfc9bc5e
DejAI1995 <ayodejiodunela@hotmail.com>
2025-05-30 17:44:57 +0100

    Initial Commit

---
+++ DejiTest/DejiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DejiTestApp.swift
+//  DejiTest
+//
+//  Created by Deji O on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DejiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DejiTestTests/DejiTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  DejiTestTests.swift
+//  DejiTestTests
+//
+//  Created by Deji O on 30/05/2025.
+//
+
+import Testing
+@testable import DejiTest
+
+struct DejiTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
